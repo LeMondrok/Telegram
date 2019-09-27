026d8a7017c478b4cdc459f5e526781a06a8b03a
lemondrok <dun2000@mail.ru>
2019-09-27 14:03:17 +0300

    test

--- start.py
+++ start.py
@@ -36,14 +36,11 @@ class BotHandler:
 
 
 greet_bot = BotHandler(token)
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')
 now = datetime.datetime.now()
 
 
 def main():
     new_offset = None
-    today = now.day
-    hour = now.hour
 
     while True:
         greet_bot.get_updates(new_offset)
@@ -54,9 +51,9 @@ def main():
             last_update_id = last_update['update_id']
             last_chat_text = last_update['message']['text']
             last_chat_id = last_update['message']['chat']['id']
-            last_chat_name = last_update['message']['chat']['first_name']
+            # last_chat_name = last_update['message']['chat']['first_name']
 
-            greet_bot.send_message(last_chat_id, 'Ты пидор')
+            greet_bot.send_message(last_chat_id, last_chat_text)
 
             new_offset = last_update_id + 1
 
